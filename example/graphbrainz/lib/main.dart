2331b7b6f4ad09243eb4d5ca56ca4a169cedbfbe
Igor Borges <igor@borges.me>
2019-06-01 00:18:37 -0300

    Update example as a good query

--- example/graphbrainz/lib/main.dart
+++ example/graphbrainz/lib/main.dart
@@ -1,9 +1,18 @@
-import 'package:graphql_builder/graphql_builder.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'graphbrainz.api.dart';
 
-final query = '''
+Future<void> main() async {
+  const graphQLEndpoint = 'https://graphbrainz.herokuapp.com/';
+  final client = http.Client();
+  final dataResponse = await client.post(graphQLEndpoint, body: {
+    'operationName': 'EdSheeran',
+    'query': '''
 query EdSheeran {
   node(id: "QXJ0aXN0OmI4YTdjNTFmLTM2MmMtNGRjYi1hMjU5LWJjNmUwMDk1ZjBhNg==") {
+    __typename
+    id
     ... on Artist {
       mbid
       name
@@ -15,5 +24,13 @@ query EdSheeran {
       }
     }
   }
+}'''
+  });
+  client.close();
+
+  final typedResponse = Query.fromJson(json.decode(dataResponse.body)['data']);
+
+  print(typedResponse.node.id);
+  // print(typedResponse.node.__typename);
+  // print((typedResponse.node as Artist).spotify.href);
 }
-''';
