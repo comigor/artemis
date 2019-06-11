fb37a5554bea43e83e7d8fbbef62b5c8966ee0d9
Igor Borges <igor@borges.me>
2019-06-10 21:03:20 -0300

    Create complex_test.dart

---
+++ test/complex_test.dart
@@ -0,0 +1,139 @@
+import 'dart:convert';
+
+import 'package:build/build.dart';
+import 'package:build_test/build_test.dart';
+import 'package:test/test.dart';
+
+import 'package:artemis/schema/graphql.dart';
+import 'package:artemis/generator.dart';
+
+String jsonFromSchema(GraphQLSchema schema) => json.encode({
+      'data': {'__schema': schema.toJson()}
+    });
+
+void main() {
+  group('On complex schemas: interfaces with unions', () {
+    final builder = graphQLTypesBuilder(BuilderOptions({}));
+
+    test(
+        'An object can implement interfaces and be part of a union type at the same time',
+        () async {
+      final GraphQLSchema schema = GraphQLSchema(types: [
+        GraphQLType(name: 'String', kind: GraphQLTypeKind.SCALAR),
+        GraphQLType(name: 'Int', kind: GraphQLTypeKind.SCALAR),
+        GraphQLType(name: 'IDable', kind: GraphQLTypeKind.INTERFACE, fields: [
+          GraphQLField(name: 'id', type: GraphQLType(name: 'String'))
+        ], possibleTypes: [
+          GraphQLType(name: 'Song'),
+        ]),
+        GraphQLType(
+            name: 'Titleable',
+            kind: GraphQLTypeKind.INTERFACE,
+            fields: [
+              GraphQLField(name: 'title', type: GraphQLType(name: 'String'))
+            ],
+            possibleTypes: [
+              GraphQLType(name: 'Song'),
+            ]),
+        GraphQLType(name: 'Song', kind: GraphQLTypeKind.OBJECT, fields: [
+          GraphQLField(name: 'id', type: GraphQLType(name: 'String')),
+          GraphQLField(name: 'title', type: GraphQLType(name: 'String')),
+          GraphQLField(name: 'duration', type: GraphQLType(name: 'Int'))
+        ], interfaces: [
+          GraphQLType(name: 'IDable'),
+          GraphQLType(name: 'Titleable'),
+        ]),
+        GraphQLType(name: 'Book', kind: GraphQLTypeKind.OBJECT, fields: [
+          GraphQLField(name: 'id', type: GraphQLType(name: 'String')),
+          GraphQLField(name: 'title', type: GraphQLType(name: 'String')),
+          GraphQLField(name: 'pages', type: GraphQLType(name: 'Int'))
+        ], interfaces: [
+          GraphQLType(name: 'IDable'),
+          GraphQLType(name: 'Titleable'),
+        ]),
+        GraphQLType(
+            name: 'Result',
+            kind: GraphQLTypeKind.UNION,
+            possibleTypes: [
+              GraphQLType(name: 'Song'),
+              GraphQLType(name: 'Book'),
+            ]),
+      ]);
+
+      await testBuilder(builder, {
+        'a|api.schema.json': jsonFromSchema(schema),
+      }, outputs: {
+        'a|api.api.dart': '''// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'api.api.g.dart';
+
+@JsonSerializable()
+class IDable {
+  String id;
+
+  IDable();
+
+  factory IDable.fromJson(Map<String, dynamic> json) => _\$IDableFromJson(json);
+  Map<String, dynamic> toJson() => _\$IDableToJson(this);
+}
+
+@JsonSerializable()
+class Titleable {
+  String title;
+
+  Titleable();
+
+  factory Titleable.fromJson(Map<String, dynamic> json) =>
+      _\$TitleableFromJson(json);
+  Map<String, dynamic> toJson() => _\$TitleableToJson(this);
+}
+
+@JsonSerializable()
+class Song extends Result implements IDable, Titleable {
+  @JsonKey(name: \'__resolveType\')
+  String resolveType;
+  @override
+  String id;
+  @override
+  String title;
+  int duration;
+
+  Song();
+
+  factory Song.fromJson(Map<String, dynamic> json) => _\$SongFromJson(json);
+  Map<String, dynamic> toJson() => _\$SongToJson(this);
+}
+
+@JsonSerializable()
+class Book extends Result implements IDable, Titleable {
+  @JsonKey(name: \'__resolveType\')
+  String resolveType;
+  @override
+  String id;
+  @override
+  String title;
+  int pages;
+
+  Book();
+
+  factory Book.fromJson(Map<String, dynamic> json) => _\$BookFromJson(json);
+  Map<String, dynamic> toJson() => _\$BookToJson(this);
+}
+
+@JsonSerializable()
+class Result {
+  @JsonKey(name: \'__resolveType\')
+  String resolveType;
+
+  Result();
+
+  factory Result.fromJson(Map<String, dynamic> json) => _\$ResultFromJson(json);
+  Map<String, dynamic> toJson() => _\$ResultToJson(this);
+}
+''',
+      });
+    });
+  });
+}
