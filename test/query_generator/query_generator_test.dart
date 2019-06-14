5636cea5094f4ea2ad5291c795861dc0d8042239
Igor Borges <igor@borges.me>
2019-06-13 22:32:44 -0300

    Create a basic test form query generation

---
+++ test/query_generator/query_generator_test.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:build/build.dart';
+import 'package:build_test/build_test.dart';
+import 'package:test/test.dart';
+
+import 'package:artemis/schema/graphql.dart';
+import 'package:artemis/generator.dart';
+
+String jsonFromSchema(GraphQLSchema schema) => json.encode({
+      'data': {'__schema': schema.toJson()}
+    });
+
+void main() {
+  group('On builder options', () {
+    test('Custom scalars can have custom parsers', () async {
+      final anotherBuilder = graphQLQueryBuilder(BuilderOptions({
+        'schema_mapping': [
+          {
+            'schema': 'api.schema.json',
+            'queries_glob': '**.query.graphql',
+          }
+        ]
+      }));
+      final GraphQLSchema schema = GraphQLSchema(
+          queryType:
+              GraphQLType(name: 'SomeObject', kind: GraphQLTypeKind.OBJECT),
+          types: [
+            GraphQLType(name: 'String', kind: GraphQLTypeKind.SCALAR),
+            GraphQLType(name: 'Int', kind: GraphQLTypeKind.SCALAR),
+            GraphQLType(
+                name: 'SomeObject',
+                kind: GraphQLTypeKind.OBJECT,
+                fields: [
+                  GraphQLField(
+                      name: 's',
+                      type: GraphQLType(
+                          name: 'String', kind: GraphQLTypeKind.SCALAR)),
+                  GraphQLField(
+                      name: 'i',
+                      type: GraphQLType(
+                          name: 'Int', kind: GraphQLTypeKind.SCALAR)),
+                ]),
+          ]);
+
+      await testBuilder(anotherBuilder, {
+        'a|api.schema.json': jsonFromSchema(schema),
+        'a|some_query.query.graphql': 'query some_query { s, i }',
+      }, outputs: {
+        'a|some_query.query.dart': '''// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'some_query.query.g.dart';
+
+@JsonSerializable()
+class SomeQuery {
+  String s;
+  int i;
+
+  SomeQuery();
+
+  factory SomeQuery.fromJson(Map<String, dynamic> json) =>
+      _\$SomeQueryFromJson(json);
+  Map<String, dynamic> toJson() => _\$SomeQueryToJson(this);
+}
+''',
+      });
+    });
+  });
+}
