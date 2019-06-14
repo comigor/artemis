f727b54b76769f13294cc9e7cf23f13e49199a1f
Igor Borges <igor@borges.me>
2019-06-14 14:00:53 -0300

    Consider multiple fields using different versions of the same object

--- test/query_generator/query_generator_test.dart
+++ test/query_generator/query_generator_test.dart
@@ -226,5 +226,109 @@ class SomeQuery {
 ''',
       });
     });
+
+    test(
+        'When multiple fields use different versions of an object, aliasing them means we\'ll alias class name as well',
+        () async {
+      final anotherBuilder = graphQLQueryBuilder(BuilderOptions({
+        'schema_mapping': [
+          {
+            'schema': 'api.schema.json',
+            'queries_glob': '**.query.graphql',
+          }
+        ]
+      }));
+      final GraphQLSchema schema = GraphQLSchema(
+          queryType: GraphQLType(name: 'Query', kind: GraphQLTypeKind.OBJECT),
+          types: [
+            GraphQLType(name: 'String', kind: GraphQLTypeKind.SCALAR),
+            GraphQLType(name: 'Query', kind: GraphQLTypeKind.OBJECT, fields: [
+              GraphQLField(
+                  name: 's',
+                  type: GraphQLType(
+                      name: 'String', kind: GraphQLTypeKind.SCALAR)),
+              GraphQLField(
+                  name: 'o',
+                  type: GraphQLType(
+                      name: 'SomeObject', kind: GraphQLTypeKind.OBJECT)),
+              GraphQLField(
+                  name: 'ob',
+                  type: GraphQLType(
+                      kind: GraphQLTypeKind.LIST,
+                      ofType: GraphQLType(
+                          name: 'SomeObject', kind: GraphQLTypeKind.OBJECT))),
+            ]),
+            GraphQLType(
+                name: 'SomeObject',
+                kind: GraphQLTypeKind.OBJECT,
+                fields: [
+                  GraphQLField(
+                      name: 'st',
+                      type: GraphQLType(
+                          name: 'String', kind: GraphQLTypeKind.SCALAR)),
+                  GraphQLField(
+                      name: 'str',
+                      type: GraphQLType(
+                          name: 'String', kind: GraphQLTypeKind.SCALAR)),
+                ]),
+          ]);
+
+      await testBuilder(anotherBuilder, {
+        'a|api.schema.json': jsonFromSchema(schema),
+        'a|some_query.query.graphql': '''
+        query some_query {
+          s
+          o {
+            st
+          }
+          anotherObject: ob {
+            str
+          }
+        }
+        ''',
+      }, outputs: {
+        'a|some_query.query.dart': '''// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'some_query.query.g.dart';
+
+@JsonSerializable()
+class SomeQuery {
+  String s;
+  SomeObject o;
+  List<AnotherObject> anotherObject;
+
+  SomeQuery();
+
+  factory SomeQuery.fromJson(Map<String, dynamic> json) =>
+      _\$SomeQueryFromJson(json);
+  Map<String, dynamic> toJson() => _\$SomeQueryToJson(this);
+}
+
+@JsonSerializable()
+class SomeObject {
+  String st;
+
+  SomeObject();
+
+  factory SomeObject.fromJson(Map<String, dynamic> json) =>
+      _\$SomeObjectFromJson(json);
+  Map<String, dynamic> toJson() => _\$SomeObjectToJson(this);
+}
+
+@JsonSerializable()
+class AnotherObject {
+  String str;
+
+  AnotherObject();
+
+  factory AnotherObject.fromJson(Map<String, dynamic> json) =>
+      _\$AnotherObjectFromJson(json);
+  Map<String, dynamic> toJson() => _\$AnotherObjectToJson(this);
+}
+''',
+      });
+    });
   });
 }
