76b3fe975404858935081c158bb0a78bf8c5ef5f
Marcelo Theodoro <marcelo.theodorojr@gmail.com>
2019-08-21 11:29:24 -0300

    Defaults response to charset utf-8
    Even if the server is responding with header `Content-Type`: `application/json; charset=utf-8`, `response.body` is converting to iso-8859, breaking encoding in ios.
    By setting the default json charset as utf-8, you prevent it from happening.

--- lib/client.dart
+++ lib/client.dart
@@ -40,7 +40,7 @@ class ArtemisClient {
       }),
     );
 
-    final Map<String, dynamic> jsonBody = json.decode(dataResponse.body);
+    final Map<String, dynamic> jsonBody = json.decode(utf8.decode(dataResponse.bodyBytes));
     final response = GraphQLResponse<T>.fromJson(jsonBody)
       ..data = query.parse(jsonBody['data'] ?? {});
 
