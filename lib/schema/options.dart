0f869f283a388f0124d62bf06fa4e679cfd6594c
Igor Borges <igor@borges.dev>
2019-09-02 16:07:20 -0300

    Add most documentation

--- lib/schema/options.dart
+++ lib/schema/options.dart
@@ -4,17 +4,26 @@ import 'package:json_annotation/json_annotation.dart';
 // would crash when importing options.dart file.
 part 'options.g2.dart';
 
+/// This generator options, gathered from `build.yaml` file.
 @JsonSerializable(fieldRename: FieldRename.snake, anyMap: true)
 class GeneratorOptions {
+  /// A custom import for this query, defined in `build.yaml`.
   @JsonKey(defaultValue: '')
   final String customParserImport;
+
+  /// If instances of [GraphQLQuery] should be generated.
   @JsonKey(defaultValue: true)
   final bool generateHelpers;
+
+  /// A list of scalar mappings.
   @JsonKey(defaultValue: [])
   final List<ScalarMap> scalarMapping;
+
+  /// A list of schema mappings.
   @JsonKey(defaultValue: [])
   final List<SchemaMap> schemaMapping;
 
+  /// Instantiate generator options.
   GeneratorOptions({
     this.customParserImport,
     this.generateHelpers = true,
@@ -22,24 +31,31 @@ class GeneratorOptions {
     this.schemaMapping = const [],
   });
 
+  /// Build options from a JSON map.
   factory GeneratorOptions.fromJson(Map<String, dynamic> json) =>
       _$GeneratorOptionsFromJson(json);
 
+  /// Convert this options instance to JSON.
   Map<String, dynamic> toJson() => _$GeneratorOptionsToJson(this);
 }
 
+/// Define a Dart type.
 @JsonSerializable()
 class DartType {
+  /// Dart type name.
   final String name;
 
+  /// Package imports related to this type.
   @JsonKey(defaultValue: <String>[])
   final List<String> imports;
 
+  /// Instantiate a Dart type.
   const DartType({
     this.name,
     this.imports = const [],
   });
 
+  /// Build a Dart type from a JSON string or map.
   factory DartType.fromJson(dynamic json) {
     if (json is String) {
       return DartType(name: json);
@@ -50,44 +66,63 @@ class DartType {
     }
   }
 
+  /// Convert this Dart type instance to JSON.
   Map<String, dynamic> toJson() => _$DartTypeToJson(this);
 }
 
+/// Maps a GraphQL scalar to a Dart type.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class ScalarMap {
+  /// The GraphQL type name.
   @JsonKey(name: 'graphql_type')
   final String graphQLType;
+
+  /// The Dart type linked to this GraphQL type.
   final DartType dartType;
   @JsonKey(defaultValue: false)
+
+  /// If custom parser would be used.
   final bool useCustomParser;
 
+  /// Instatiates a scalar mapping.
   ScalarMap({
     this.graphQLType,
     this.dartType,
     this.useCustomParser = false,
   });
 
+  /// Build a scalar mapping from a JSON map.
   factory ScalarMap.fromJson(Map<String, dynamic> json) =>
       _$ScalarMapFromJson(json);
 
+  /// Convert this scalar mapping instance to JSON.
   Map<String, dynamic> toJson() => _$ScalarMapToJson(this);
 }
 
+/// Maps a GraphQL schema to queries files.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class SchemaMap {
+  /// The GraphQL schema string.
   final String schema;
+
+  /// A [Glob] to find queries files.
   final String queriesGlob;
+
+  /// The resolve type field used on this schema.
   @JsonKey(defaultValue: '__resolveType')
   final String resolveTypeField;
 
+  /// Instatiates a schema mapping.
   SchemaMap({
     this.schema,
     this.queriesGlob,
     this.resolveTypeField = '__resolveType',
   });
 
+  /// Build a schema mapping from a JSON map.
   factory SchemaMap.fromJson(Map<String, dynamic> json) =>
       _$SchemaMapFromJson(json);
 
+  /// Convert this schema mapping instance to JSON.
   Map<String, dynamic> toJson() => _$SchemaMapToJson(this);
 }
