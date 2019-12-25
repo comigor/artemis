515751b03a64036cb6834d646a974b1a4cb8db1c
Igor Borges <igor@borges.dev>
2019-12-24 21:31:58 -0300

    Add gql_exec and pass through its changes

--- lib/schema/graphql_response.dart
+++ lib/schema/graphql_response.dart
@@ -1,4 +1,4 @@
-import 'package:gql/execution.dart';
+import 'package:gql_exec/gql_exec.dart';
 import 'package:meta/meta.dart';
 
 /// Encapsulates a GraphQL query/mutation response from server, with typed
