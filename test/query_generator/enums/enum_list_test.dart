5a2ab0b46543288449f7b13a838182ead99c09f3
Igor Borges <igor@borges.dev>
2020-02-20 21:04:46 -0300

    Remove line from test

--- test/query_generator/enums/enum_list_test.dart
+++ test/query_generator/enums/enum_list_test.dart
@@ -101,7 +101,6 @@ const generatedFile = r'''// GENERATED CODE - DO NOT MODIFY BY HAND
 import 'package:json_annotation/json_annotation.dart';
 import 'package:equatable/equatable.dart';
 import 'package:gql/ast.dart';
-import 'package:meta/meta.dart';
 part 'query.g.dart';
 
 @JsonSerializable(explicitToJson: true)
