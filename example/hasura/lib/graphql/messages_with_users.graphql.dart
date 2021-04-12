6abbd1ce48ab3edfdb16285961f6da0cc70c005d
vasilich <vasilich@grafin.kiev.ua>
2021-04-12 13:26:50 +0300

    hasura example fix

--- example/hasura/lib/graphql/messages_with_users.graphql.dart
+++ example/hasura/lib/graphql/messages_with_users.graphql.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// @dart = 2.12
 
-import 'package:meta/meta.dart';
 import 'package:artemis/artemis.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:equatable/equatable.dart';
@@ -8,117 +8,126 @@ import 'package:gql/ast.dart';
 part 'messages_with_users.graphql.g.dart';
 
 @JsonSerializable(explicitToJson: true)
-class MessagesWithUsers$SubscriptionRoot$Messages$Profile with EquatableMixin {
+class MessagesWithUsers$SubscriptionRoot$Messages$Profile
+    extends JsonSerializable with EquatableMixin {
   MessagesWithUsers$SubscriptionRoot$Messages$Profile();
 
   factory MessagesWithUsers$SubscriptionRoot$Messages$Profile.fromJson(
           Map<String, dynamic> json) =>
       _$MessagesWithUsers$SubscriptionRoot$Messages$ProfileFromJson(json);
 
-  int id;
+  late int id;
 
-  String name;
+  late String name;
 
   @override
-  List<Object> get props => [id, name];
+  List<Object?> get props => [id, name];
+
   Map<String, dynamic> toJson() =>
       _$MessagesWithUsers$SubscriptionRoot$Messages$ProfileToJson(this);
 }
 
 @JsonSerializable(explicitToJson: true)
-class MessagesWithUsers$SubscriptionRoot$Messages with EquatableMixin {
+class MessagesWithUsers$SubscriptionRoot$Messages extends JsonSerializable
+    with EquatableMixin {
   MessagesWithUsers$SubscriptionRoot$Messages();
 
   factory MessagesWithUsers$SubscriptionRoot$Messages.fromJson(
           Map<String, dynamic> json) =>
       _$MessagesWithUsers$SubscriptionRoot$MessagesFromJson(json);
 
-  int id;
+  late int id;
 
-  String message;
+  late String message;
 
-  MessagesWithUsers$SubscriptionRoot$Messages$Profile profile;
+  late MessagesWithUsers$SubscriptionRoot$Messages$Profile profile;
 
   @override
-  List<Object> get props => [id, message, profile];
+  List<Object?> get props => [id, message, profile];
+
   Map<String, dynamic> toJson() =>
       _$MessagesWithUsers$SubscriptionRoot$MessagesToJson(this);
 }
 
 @JsonSerializable(explicitToJson: true)
-class MessagesWithUsers$SubscriptionRoot with EquatableMixin {
+class MessagesWithUsers$SubscriptionRoot extends JsonSerializable
+    with EquatableMixin {
   MessagesWithUsers$SubscriptionRoot();
 
   factory MessagesWithUsers$SubscriptionRoot.fromJson(
           Map<String, dynamic> json) =>
       _$MessagesWithUsers$SubscriptionRootFromJson(json);
 
-  List<MessagesWithUsers$SubscriptionRoot$Messages> messages;
+  late List<MessagesWithUsers$SubscriptionRoot$Messages> messages;
 
   @override
-  List<Object> get props => [messages];
+  List<Object?> get props => [messages];
+
   Map<String, dynamic> toJson() =>
       _$MessagesWithUsers$SubscriptionRootToJson(this);
 }
+
+final MESSAGES_WITH_USERS_SUBSCRIPTION_DOCUMENT = DocumentNode(definitions: [
+  OperationDefinitionNode(
+      type: OperationType.subscription,
+      name: NameNode(value: 'messages_with_users'),
+      variableDefinitions: [],
+      directives: [],
+      selectionSet: SelectionSetNode(selections: [
+        FieldNode(
+            name: NameNode(value: 'messages'),
+            alias: null,
+            arguments: [],
+            directives: [],
+            selectionSet: SelectionSetNode(selections: [
+              FieldNode(
+                  name: NameNode(value: 'id'),
+                  alias: null,
+                  arguments: [],
+                  directives: [],
+                  selectionSet: null),
+              FieldNode(
+                  name: NameNode(value: 'message'),
+                  alias: null,
+                  arguments: [],
+                  directives: [],
+                  selectionSet: null),
+              FieldNode(
+                  name: NameNode(value: 'profile'),
+                  alias: null,
+                  arguments: [],
+                  directives: [],
+                  selectionSet: SelectionSetNode(selections: [
+                    FieldNode(
+                        name: NameNode(value: 'id'),
+                        alias: null,
+                        arguments: [],
+                        directives: [],
+                        selectionSet: null),
+                    FieldNode(
+                        name: NameNode(value: 'name'),
+                        alias: null,
+                        arguments: [],
+                        directives: [],
+                        selectionSet: null)
+                  ]))
+            ]))
+      ]))
+]);
 
 class MessagesWithUsersSubscription
     extends GraphQLQuery<MessagesWithUsers$SubscriptionRoot, JsonSerializable> {
   MessagesWithUsersSubscription();
 
   @override
-  final DocumentNode document = DocumentNode(definitions: [
-    OperationDefinitionNode(
-        type: OperationType.subscription,
-        name: NameNode(value: 'messages_with_users'),
-        variableDefinitions: [],
-        directives: [],
-        selectionSet: SelectionSetNode(selections: [
-          FieldNode(
-              name: NameNode(value: 'messages'),
-              alias: null,
-              arguments: [],
-              directives: [],
-              selectionSet: SelectionSetNode(selections: [
-                FieldNode(
-                    name: NameNode(value: 'id'),
-                    alias: null,
-                    arguments: [],
-                    directives: [],
-                    selectionSet: null),
-                FieldNode(
-                    name: NameNode(value: 'message'),
-                    alias: null,
-                    arguments: [],
-                    directives: [],
-                    selectionSet: null),
-                FieldNode(
-                    name: NameNode(value: 'profile'),
-                    alias: null,
-                    arguments: [],
-                    directives: [],
-                    selectionSet: SelectionSetNode(selections: [
-                      FieldNode(
-                          name: NameNode(value: 'id'),
-                          alias: null,
-                          arguments: [],
-                          directives: [],
-                          selectionSet: null),
-                      FieldNode(
-                          name: NameNode(value: 'name'),
-                          alias: null,
-                          arguments: [],
-                          directives: [],
-                          selectionSet: null)
-                    ]))
-              ]))
-        ]))
-  ]);
+  final DocumentNode document = MESSAGES_WITH_USERS_SUBSCRIPTION_DOCUMENT;
 
   @override
   final String operationName = 'messages_with_users';
 
   @override
-  List<Object> get props => [document, operationName];
+  List<Object?> get props => [document, operationName];
+
   @override
   MessagesWithUsers$SubscriptionRoot parse(Map<String, dynamic> json) =>
       MessagesWithUsers$SubscriptionRoot.fromJson(json);
